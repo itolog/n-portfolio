4eee03697904713430ac5fa4cd28b089d8a51b2a
itolog <paukanlove@gmail.com>
2023-02-05 13:25:20 +0200

    add gsap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   },
   css: ["vuetify/styles", "@/assets/styles/index.scss"],
   build: {
-    transpile: ["vuetify"],
+    transpile: ["vuetify", "gsap"],
   },
   vite: {
     plugins: [
