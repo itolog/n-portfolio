724515c63386f82201d07b02e5e80d121c20697d
itolog <paukanlove@gmail.com>
2023-02-11 16:22:03 +0200

    create custom cursor wrapper

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,15 @@
 <template>
-  <div class="default-layout">
-    <!--    <div ref="cursor" id="cursor"/>-->
-    <!--    <div ref="aura" id="aura"/>-->
+  <CustomMouse class="default-layout">
     <AppHeader/>
     <main class="app-main">
       <slot/>
     </main>
     <AppFooter/>
-  </div>
+  </CustomMouse>
 </template>
 
 <script setup lang="ts">
-// import { ref } from "vue";
 import { onMounted, usePageTransition } from "#imports";
-
-// const cursor = ref<HTMLDivElement | null>(null);
-// const aura = ref<HTMLDivElement | null>(null);
-
-// const { mouseCoords, mouseOut } = useCursor({
-//   auraRef: aura,
-//   cursorRef: cursor
-// });
 
 useDefaultLayoutAnimation();
 const { togglePageTransition } = usePageTransition();
@@ -31,7 +20,6 @@ onMounted(() => {
 </script>
 
 <style scoped lang="scss">
-@import "assets/styles/abstarcts/mixins_media";
 
 .default-layout {
   display: grid;
@@ -44,49 +32,4 @@ onMounted(() => {
   padding: 10px;
   opacity: 0;
 }
-
-#cursor, #aura {
-  position: absolute;
-  border-radius: 100%;
-  transition: .5s cubic-bezier(.75, -1.27, .3, 2.33) transform, .2s cubic-bezier(.75, -1.27, .3, 2.33) opacity;
-  user-select: none;
-  pointer-events: none;
-  z-index: 10000;
-
-  @include laptop() {
-    display: none;
-  }
-}
-
-#cursor {
-  width: 8px;
-  height: 8px;
-  background-color: rgba(240, 248, 255, 0.5);
-  transform: scale(1);
-
-  &.active {
-    opacity: 0;
-    transform: scale(0);
-  }
-}
-
-#aura {
-  width: 40px;
-  height: 40px;
-  border: 1px solid rgba(240, 248, 255, 0.2);
-  background-color: rgba(240, 248, 255, 0.27);
-  transform: translate(5px, 5px);
-
-  &.active {
-    background: #fff;
-    mix-blend-mode: difference;
-    border: 2px solid rgba(240, 248, 255, 0.3);
-    transform: scale(1.1);
-  }
-
-  &.hidden {
-    transform: scale(.001)
-  }
-}
-
 </style>
